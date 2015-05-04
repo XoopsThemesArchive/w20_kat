6c4ad6578c63f4038aca66b56dfb0aa0357d2102
mambax7 <mambax7@gmail.com>
2015-05-04 05:50:28 -0400

    Original files

---
+++ w20_kat/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "w20_kat";
+	$txc_themeinfo["author"] = "azpilot";
+	$txc_themeinfo["author_url"] = "http://mypilottalk.com";
+	$txc_themeinfo["detail_url"] = "http://katthemes.com/modules/PDdownloads";
+	$txc_themeinfo["download_url"] = "http://katthemes.com/modules/PDdownloads";
+?>
